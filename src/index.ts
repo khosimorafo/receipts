bbe6489bfd7435560db766787deef6a84408fd5d
Khosi Morafo <khosimorafo@yahoo.com>
2020-04-12 12:03:41 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 export { run } from "@oclif/command";
+
 import Twilio from "./utils/twilio";
 
 var express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+/* eslint-disable no-console */
 
+const port = process.env.PORT || 3000;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 let t = new Twilio();
 
 app.get("/", function(req: any, res: any) {
-  res.send("Uyaphi lapho?");
+  res.render("index");
 });
 
 app.post("/twilio/send", function(req: any, res: any) {
@@ -29,6 +37,12 @@ app.get("/twilio/validate", function(req: any, res: any) {
   });
 });
 
-app.listen(3000, () => console.log(`Started server at http://localhost:3000`));
+app.listen(port, (err: any) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`App server running at: http://localhost:${port}`);
+  }
+});
 
 export default app;
