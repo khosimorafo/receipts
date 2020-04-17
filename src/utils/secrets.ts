5e904b8430f0e0e1839619068dcf9757b5970418
Khosi Morafo <khosimorafo@yahoo.com>
2020-04-17 17:19:35 +0200

    first commit

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -4,14 +4,6 @@ const client = new SecretManagerServiceClient();
 
 const project_id = process.env["PROJECT_ID"];
 
-async function testFunction(isOK: boolean) {
-  if (isOK === true) {
-    return "OK";
-  } else {
-    throw new Error("not OK");
-  }
-}
-
 async function getSecret(key: string, version: string) {
   const parent = `projects/${project_id}/secrets/${key}`;
   const name = `${parent}/versions/${version}`;
@@ -22,12 +14,11 @@ async function getSecret(key: string, version: string) {
 
   // Extract the payload as a string.
   const payload = result.payload.data.toString("utf8");
-  console.info(`Payload: ${payload}`);
+  //console.info(`Payload: ${payload}`);
   return payload;
 }
 
 module.exports = {
-  testFunction,
   getSecret
 };
 
